6eb26a314c1fd05f7b4442309d0b0fc1ebb713bf
johnny lamb <agnusjack@gmail.com>
2015-09-08 22:54:56 -0600

    added routes put and delete

--- server/routes/index.js
+++ server/routes/index.js
@@ -17,7 +17,6 @@ router.get('/bugs', function(req, res, next) {
     }
   });
 });
-
 // get single bug
 router.get('/bug/:id', function(req, res, next) {
   Bug.findById(req.params.id,function(err, data) {
@@ -30,8 +29,6 @@ router.get('/bug/:id', function(req, res, next) {
     }
   });
 });
-
-
 // POST request for create a new bug
 router.post('/bugs', function(req, res, next) {
   newBug = new Bug({
@@ -48,6 +45,33 @@ router.post('/bugs', function(req, res, next) {
     }
   });
 });
+// put for single bug
+router.put('/bug/:id', function(req, res, next) {
+  var update = {
+    name: req.body.name,
+    age: req.body.age
+  };
+  Bug.findByIdAndUpdate(req.params.id, update,function(err, data) {
+    if (err) {
+      res.json({
+        'message': err
+      });
+    } else {
+      res.json(data);
+    }
+  });
+});
 
-
+// DELETE request for single bug
+router.put('/bug/:id', function(req, res, next) {
+  Bug.findByIdAndRemove(req.params.id,function(err, data) {
+    if (err) {
+      res.json({
+        'message': err
+      });
+    } else {
+      res.json(data);
+    }
+  });
+});
 module.exports = router;
